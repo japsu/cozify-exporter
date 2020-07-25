9bdedcec4e38b7c426dbf69db01497dff4625188
Santtu Pajukanta <santtu@pajukanta.fi>
2020-07-25 19:05:00 +0300

    fix indentation

--- cozify_exporter.py
+++ cozify_exporter.py
@@ -33,8 +33,8 @@ COUNTERS = [
 
 # https://stackoverflow.com/a/1176023/1012299
 def camel_to_snake(key):
-  key = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', key)
-  return re.sub('([a-z0-9])([A-Z])', r'\1_\2', key).lower()
+    key = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', key)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', key).lower()
 
 
 @app.route('/metrics')
